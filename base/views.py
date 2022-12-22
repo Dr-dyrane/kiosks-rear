e1864368eed07c5c35c0841d8da6633cf58d49f3
Dr-dyrane <halodyrane@gmail.com>
2022-12-22 12:12:36 +0100

    kiosks backend

---
+++ base/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from base.models import *
+from base.serializers import *
+from base.products import products
+
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+  routes = [
+    '/api/products/',
+    '/api/products/create/',
+    '/api/products/upload/',
+    '/api/products/<id>/reviews/',
+    '/api/products/<id>/',
+    '/api/products/delete/<id>/',
+    '/api/products/<update>/<id>/',
+    '/api/products/top/',
+    ]
+  return Response(routes)
+  
+@api_view(['GET','UPDATE','POST'])
+def getProducts(request):
+  products = Product.objects.all()
+  serializer = ProductSerializer(products, many=True)
+  return Response(serializer.data)
+
+@api_view(['GET'])  
+def getProduct(request,pk):
+  product = Product.objects.get(_id=pk)
+  serializer = ProductSerializer(product, many=False)
+  return Response(serializer.data)
